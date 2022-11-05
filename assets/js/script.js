fb3c70cb9b387e29513064e36c97f5f484fb8e58
krgurevich <kananyan@optonline.net>
2022-11-05 16:55:07 -0400

    updated JS code with div styles and fixed duplication of historyArr

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,12 @@ function searchFormSubmit(event) {
         return;
     }
 
-    historyArr.push(cityInput);
+    let cityName = historyArr.filter((element) => element === cityInput)
+    if (cityName.length === 0) {
+        historyArr.push(cityInput);
+    }
+
+    console.log(historyArr);
     showHistory();
 
 
@@ -43,17 +48,17 @@ function searchFormSubmit(event) {
         .then(data => {
             console.log(data);
             const resultHTML = `
-            <h3>
+            <h2>
                 ${data.name} (${new Date().toLocaleDateString()}) 
-                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
-            </h3>
-            <p><strong>Temp: </strong> ${data.main.temp}</p>
-            <p><strong>Wind: </strong> ${data.wind.speed}</p>
-            <p><strong>Humidity: </strong> ${data.main.humidity}</p>
+                <img class="img-icon" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </h2>
+            <p class="sub-title">Temp: ${data.main.temp} &#8457</p>
+            <p class="sub-title">Wind: ${data.wind.speed} MPH</p>
+            <p class="sub-title">Humidity: ${data.main.humidity} &#37;</p>
             `
             resultTodayEl.innerHTML = resultHTML;
+            resultTodayEl.style.marginTop = "15px"
         })
-
 
 
     var queryForecast = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + "&units=imperial" + "&appid=" + apiKey;
@@ -62,35 +67,39 @@ function searchFormSubmit(event) {
         .then(response => response.json())
         .then(data => {
             for (var i = 0; i < data.list.length; i += 8) {
+                // i += 8 => i = i + 8
                 console.log(data.list[i]);
                 const listItem = data.list[i]
                 const resultDiv = document.createElement("div");
                 const resultHTML = `
-            <h3 class="h3html">
+            <h2>
                 ${new Date(listItem.dt_txt).toLocaleDateString()} 
-                <img src="http://openweathermap.org/img/w/${listItem.weather[0].icon}.png">
-            </h3>
-            <p><strong>Temp: </strong> ${listItem.main.temp}</p>
-            <p><strong>Wind: </strong> ${listItem.wind.speed}</p>
-            <p><strong>Humidity: </strong> ${listItem.main.humidity}</p>
+                <img class="img-icon" src="http://openweathermap.org/img/w/${listItem.weather[0].icon}.png">
+            </h2>
+            <p class="sub-title">Temp: ${listItem.main.temp} &#8457</p>
+            <p class="sub-title">Wind: ${listItem.wind.speed} MPH</p>
+            <p class="sub-title">Humidity: ${listItem.main.humidity} &#37;</p>
             `
-                resultDiv.classList.add("class")
+
                 resultDiv.innerHTML = resultHTML;
+                resultForecastEl.style.display = "flex";
+                resultForecastEl.style.flexWrap = "wrap";
                 resultForecastEl.appendChild(resultDiv);
+                resultDiv.style.justifyContent = "space-between";
+                resultDiv.style.flexBasis = "calc(20% - 5px)";
             }
         });
     console.log(historyArr);
 
 }
 
-
 function showHistory() {
     for (var i = 0; i < historyArr.length; i++) {
         const historyBtn = document.createElement("button")
         historyBtn.textContent = historyArr[i];
-        historyBtn.addEventListener("click", function () {
+        historyBtn.addEventListener("click", function (e) {
             document.querySelector("#city").value = historyBtn.textContent;
-            searchFormSubmit(event);
+            searchFormSubmit(e);
         })
 
         searchHistoryCont.appendChild(historyBtn);
